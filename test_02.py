f25e47549fbfb485fdfe388334b44e967708b473
김현희 <tta_bi@naver.com>
2022-02-07 11:06:53 +0900

    배팅 갯수만큼 코인이 이동하도록 처리

--- test_02.py
+++ test_02.py
@@ -5,8 +5,8 @@ import random
 #  => 내가 가진 코인 갯수보다 많이 배팅은 불가
 # 배팅한 갯수만큼 코인이 이동하도록
 
-user_coin = 20
-cpu_coin = 20
+user_coin = 10
+cpu_coin = 10
 
 while True:
     
@@ -48,25 +48,30 @@ while True:
             
             print('사용자 승리입니다.')
             
-            # 사용자 승리 => 코인 한 개 CPU로부터 받아오자
-            user_coin = user_coin + cpu_count
-            cpu_coin = cpu_coin - cpu_count
+            # 사용자 승리 => 코인을 배팅한 만큼 CPU로부터 받아오자
+            user_coin += user_bet_coin
+            cpu_coin -= user_bet_coin
         else:
             print('사용자 패배입니다.')
-            user_coin = user_coin - cpu_count
-            cpu_coin = cpu_coin + cpu_count
+            user_coin -= user_bet_coin
+            cpu_coin += user_bet_coin
     else:
         # 짝을 입력한 경우
         if cpu_count % 2 == 0 :
             
             print('사용자 승리입니다.')
             # 맞춘 경우
-            user_coin = user_coin + cpu_count
-            cpu_coin = cpu_coin - cpu_count
+            user_coin += user_bet_coin
+            cpu_coin -= user_bet_coin
         else:
             print('사용자 패배입니다.')
-            user_coin = user_coin - cpu_count
-            cpu_coin = cpu_coin + cpu_count
+            user_coin -= user_bet_coin
+            cpu_coin += user_bet_coin
+            
+    # 현재 각자의 코인 갯수도 출력
+    
+    print(f'사용자 보유 코인 : {user_coin}개')
+    print(f'CPU 보유 코인 : {cpu_coin}개')
             
     # 둘 중 하나의 코인이 다 떨어졌다면? => 경기 종료
     
